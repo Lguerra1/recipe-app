8d39751ffe422bd7d51a6ca76f6afae5e43dca85
larry <nightmachine@gmail.com>
2021-08-25 20:44:10 -0600

    created shopping list section

--- src/app/shoppingListLayout/shoppingList/shoppingList.component.ts
+++ src/app/shoppingListLayout/shoppingList/shoppingList.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shoppingList',
   templateUrl: './shoppingList.component.html',
   styleUrls: ['./shoppingList.component.scss'],
 })
-export class ShoppingList {
-    
+export class ShoppingList implements OnInit {
+  public ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Oranges', 9),
+    new Ingredient('Bannanas', 6),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
 }
